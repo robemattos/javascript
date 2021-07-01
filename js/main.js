e90357ba3e6b96747b3f1ca9424d8a569955024c
Robson <robemattos83@gmail.com>
2021-06-30 22:19:27 -0300

    Introdução a programação javascript

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+var nome="Robson Mattos";
+var n1=51;
+var n2=10;
+var frase="Flamengo é o melhor time do mundo";
+/*alert(nome + " tem " + idade + " anos");
+alert(idade + idade2);*/
+console.log(nome);
+console.log(n1 - n2); //operadores matemáticos (+, -, *, /)
+//console.log(frase.replace("Flamengo", "Brasil")); troca a string
+//alert(frase.replace("Flamengo", "Brasil")); troca a string
+//console.log(frase.toUpperCase()); converte a string para maiúsculas - F12 para acessar o console no navegador
+//console.log(frase.toLowerCase()); converte a string para minúsculas - F12 para acessar o console no navegador
