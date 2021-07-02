916259f20cbf9eba7a5b62103389424ca75332d0
Robson <robemattos83@gmail.com>
2021-07-01 22:07:20 -0300

    aprendendo array e dicionário

--- js/main.js
+++ js/main.js
@@ -1,12 +1,37 @@
 
-var nome="Robson Mattos";
-var n1=51;
-var n2=10;
+//DICIONÁRIO
+
+var frutas = [{nome: "maça", cor: "vermelha"}, {nome: "uva", cor: "roxa"}];
+console.log(frutas);
+alert(frutas[1].nome);
+
+
+
+/*var fruta = {nome: "maça", cor: "vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+//LISTA
+//var lista = ["maça", "pêra", "laranja"];
+//lista.push("uva"); //acrescenta um elemento na lista
+//lista.pop(); //retira o último elemento da lista
+//console.log(lista); //exibe a lista no console
+//console.log(lista.length); //mostra o tamanho da lista
+//console.log(lista.reverse()); //imprime a lista em ordem reversa
+//console.log(lista.toString()); //converte o array para string
+//console.log(lista[0]); //imprime o primeiro elemento da lista
+//console.log(lista.toString()[0]); //imprime a primeira letra da string
+//console.log(lista.join(" | ")); //altera o separador padrão da lista (virgula) para o que o usuário escolher
+
+//var nome="Robson Mattos";
+//var n1=51;
+//var n2=10;
 var frase="Flamengo é o melhor time do mundo";
 /*alert(nome + " tem " + idade + " anos");
 alert(idade + idade2);*/
-console.log(nome);
-console.log(n1 - n2); //operadores matemáticos (+, -, *, /)
+//console.log(nome);
+//console.log(n1 - n2); //operadores matemáticos (+, -, *, /)
 //console.log(frase.replace("Flamengo", "Brasil")); troca a string
 //alert(frase.replace("Flamengo", "Brasil")); troca a string
 //console.log(frase.toUpperCase()); converte a string para maiúsculas - F12 para acessar o console no navegador
